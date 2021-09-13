07a812f0c403199a241c81e4834bb0a0a8252633
lee-tae-eon <mktears0609@gmail.com>
2021-09-13 23:02:27 +0900

    jwt && mail spec test done 9.0

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -135,7 +135,7 @@ export class UsersService {
       }
       return { ok: false, error: 'verification not found' };
     } catch (error) {
-      return { ok: false, error };
+      return { ok: false, error: 'could not verify email' };
     }
   }
 }
